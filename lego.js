f9c92f6a00d9944a60e72df74d873e16082e61d4
oraclespainpresales <oraclespainpresales@gmail.com>
2019-02-20 00:10:57 +0100

    fix lego

--- lego.js
+++ lego.js
@@ -31,6 +31,12 @@ module.exports = {
 	        description: "Get image version",
 	        command: "cat /version"
 	      },
+				{
+					verb: "GET",
+	        action: "ping",
+	        description: "Ping the RPi",
+	        command: "echo pong"
+	      },
 	    ]
 		},
 		{
